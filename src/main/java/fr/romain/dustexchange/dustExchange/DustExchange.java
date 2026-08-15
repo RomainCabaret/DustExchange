3e53754956f9a60b05a8bc55e48d2fd3168ad084
Romain <romainc.cabaret@gmail.com>
2026-08-15 17:47:08 +0200

    init projet

---
+++ src/main/java/fr/romain/dustexchange/dustExchange/DustExchange.java
@@ -0,0 +1,16 @@
+package fr.romain.dustexchange.dustExchange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DustExchange extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
